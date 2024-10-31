a4c194e4cdb35cec4b8a82613c9091790bbf2a64
YamelyLeonCarpio1 <yamelyleon4@gmail.com>
2024-10-31 15:52:43 -0500

    primer commit

---
+++ src/main/java/com/leoncarpio/main/Apirfds220242Application.java
@@ -0,0 +1,13 @@
+package com.leoncarpio.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apirfds220242Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apirfds220242Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/leoncarpio/main/Apirfds220242ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoncarpio.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apirfds220242ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
